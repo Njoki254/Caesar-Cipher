810f028e7bacb5f1bfd103f1c50cfd64835dcf63
Njoki Kamau <njokifaith61@gmail.com>
2020-06-30 14:01:02 +0300

    added more files and their test to separate behaviors

--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -1,4 +1,19 @@
 public class Encoding {
 
-    
+    public static String encode(String plainText, int shift) {
+        //for when user puts number greater than 26  or a negative number
+        if (shift > 26) {
+            shift = shift % 26;
+        } else if (shift < 0) {
+            shift = (shift % 26) + 26;
+        }
+       String encodedText = "";
+        int length = plainText.length();
+        for(int i= 0; i<length; i++){
+            char ch = plainText.charAt(i);
+            if(Character.isLetter(ch)){
+        }
+        return encodedText;
+
+    }
 }
