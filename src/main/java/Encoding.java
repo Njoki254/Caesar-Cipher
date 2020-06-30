5297f1b29b77e231810002460b4a9d1c24bc2ec5
Njoki Kamau <njokifaith61@gmail.com>
2020-06-30 14:22:06 +0300

    import LowerCase and uppercase methods from Junit

--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -1,18 +1,29 @@
+import java.util.Scanner;
+
+import static java.lang.Character.isLowerCase;
+import static java.lang.Character.isUpperCase;
+
 public class Encoding {
 
-    public static String encode(String plainText, int shift) {
+    public static String encode(String plainText, int shiftBy) {
         //for when user puts number greater than 26  or a negative number
-        if (shift > 26) {
-            shift = shift % 26;
-        } else if (shift < 0) {
-            shift = (shift % 26) + 26;
+        if (shiftBy > 26) {
+            shiftBy = shiftBy % 26;
+        } else if (shiftBy < 0) {
+            shiftBy = (shiftBy % 26) + 26;
         }
        String encodedText = "";
         int length = plainText.length();
         for(int i= 0; i<length; i++){
             char ch = plainText.charAt(i);
             if(Character.isLetter(ch)){
-                
+                if(Character.isLowerCase(ch)){
+                    char c = (char)(ch+shiftBy);
+                    if()
+                }
+                else(Character.isUpperCase(ch)){
+
+                }
             }
             else{
                 encodedText += ch;
