19ee5e698d759774469c6d57afb88020be47c292
Njoki Kamau <njokifaith61@gmail.com>
2020-06-30 13:59:10 +0300

    initial commit and wrote test to test functionality of cipher

---
+++ src/main/java/FrontEnd.java
@@ -0,0 +1,8 @@
+public class FrontEnd {
+    public static  String[] testPractice(){
+        String[] x = {"The test is working", "Lets proceed"};
+        return x;
+    }
+
+
+}
---
+++ src/test/java/DecodingTest.java
@@ -0,0 +1,5 @@
+import junit.framework.TestCase;
+
+public class DecodingTest extends TestCase {
+
+}
