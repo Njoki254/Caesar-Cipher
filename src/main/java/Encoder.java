5a49f271cc53a030b52171b91f9f10db949513ef
Njoki Kamau <njokifaith61@gmail.com>
2020-07-01 21:08:45 +0300

    adding and testing function of producing length of message

--- src/main/java/Encoder.java
+++ src/main/java/Encoder.java
@@ -2,7 +2,7 @@ import java.lang.*;
 
 public class Encoder{
 //two parameters needed, plainText= code being encrypted, shiftby is the number
-    public String EncodeMessage(String message, int shiftBy) {
+    public String ValidateUserInput(String message, int shiftBy) {
 //validation part of the code in case of - or # greater than 26, for unwanted user input
         if (shiftBy > 26) {
             shiftBy = shiftBy % 26;
@@ -12,4 +12,15 @@ public class Encoder{
         String tester = "It's working";
         return tester;
     }
+    public Integer calculateLength (String message){
+        //the return value
+        String secretText = "";
+        //.length gives the number of characters in a word, message in this case
+        // integer created and called length and assigned value
+        //get length to know how many times the shift method needs to loop through
+        int length = message.length();
+
+        return length;
+    }
+
 }
--- src/test/java/EncoderTest.java
+++ src/test/java/EncoderTest.java
@@ -1,14 +1,22 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.lang.*;
+
 
 public class EncoderTest {
     Encoder cipher = new Encoder();
 
     @Test
-    public void testEncodeCipher_EncodesStrings(){
-        ;
-        assertEquals("It's working", cipher.EncodeMessage("",4));
+    public void ValidateInput_validationTestForUserInput(){
+
+        assertEquals("It's working", cipher.ValidateUserInput("",4));
 
     }
+    @Test
+    public void testCalculateLength(){
+        int message = 7;
+        assertEquals(new Integer(message),cipher.calculateLength("message"));
+    }
+
 
 }
